db8d5405df23fc8a5bb1aa1adce0117b4d158145
Felipe Noronha <felipe.dariva07@gmail.com>
2024-08-16 11:17:13 -0300

    aprendendo JavaScript

---
+++ script.js
@@ -0,0 +1,10 @@
+let nome = prompt("Digite seu nome.");
+alert("Olá "+nome+", tudo bem?");
+
+//alerta na tela
+alert("Olá Mundo!")
+
+//console do navegador
+console.log("Teste de console")
+
+document.getElementById("demo").innerHTML= "Felipe Noronha"
